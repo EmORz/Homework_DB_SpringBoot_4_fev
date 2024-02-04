a7fc3c3bd2f14b75a84f6b3ebc8a6edd1d0a8bbd
Emanuil <eiv.razgrad@gmail.com>
2024-02-04 21:55:44 +0200

    Must greate post request and save in DB.

--- src/main/java/com/example/Homework_DB_SpringBoot_4_fev/AlbumController.java
+++ src/main/java/com/example/Homework_DB_SpringBoot_4_fev/AlbumController.java
@@ -1,14 +1,29 @@
 package com.example.Homework_DB_SpringBoot_4_fev;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/albums")
 public class AlbumController {
 
     private AlbumRepository albumRepository;
 
     public AlbumController(AlbumRepository albumRepository) {
         this.albumRepository = albumRepository;
+    }
+
+    @PostMapping("/add")
+    public Album addAlbum(){
+
+        return null;
+    }
+
+    @GetMapping("/get")
+    public Iterable<Album> getAllAlbums(){
+        return  albumRepository.findAll();
     }
 
 
